5c51968a6f157623684bff1790eeb37098c48a73
“Adrian <greatest14@interia.pl>
2016-02-27 17:48:36 +0100

    Zmiany w CSS

--- app/cache/dev/twig/29/2990925d4d3878f6d53eb0598db92a66c6306ed2c3a8b9f03d2a892be5273ec2.php
+++ app/cache/dev/twig/29/2990925d4d3878f6d53eb0598db92a66c6306ed2c3a8b9f03d2a892be5273ec2.php
@@ -15,26 +15,28 @@ class __TwigTemplate_0d494d0d29d44759e1019e434fbc6001a371e6f6272c7a0678ce429fe4a
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_2b6e3934cfdf39899289762d6ed98143b3c4c80042b0777f096d0b1a06c6dee3 = $this->env->getExtension("native_profiler");
-        $__internal_2b6e3934cfdf39899289762d6ed98143b3c4c80042b0777f096d0b1a06c6dee3->enter($__internal_2b6e3934cfdf39899289762d6ed98143b3c4c80042b0777f096d0b1a06c6dee3_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "default/styles.html.twig"));
+        $__internal_8be59edeb7c103ee4239d9248feb45ef5d2fe6c51fe8501025790fc5f29667cf = $this->env->getExtension("native_profiler");
+        $__internal_8be59edeb7c103ee4239d9248feb45ef5d2fe6c51fe8501025790fc5f29667cf->enter($__internal_8be59edeb7c103ee4239d9248feb45ef5d2fe6c51fe8501025790fc5f29667cf_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "default/styles.html.twig"));
 
         // line 1
         echo "<style>
-div.page {
-\t
+body {
+\tbackground-color: #e9eaed;
+
 
 }
 div.header {
+\tfont-family:Calibri;
 \tpadding-bottom: 200;
 \twidth: 800;
-\tbackground-color: lightblue;
+\tbackground-color: #ffffff;
 \ttext-align: justify;
 }
 
 
 </style>";
         
-        $__internal_2b6e3934cfdf39899289762d6ed98143b3c4c80042b0777f096d0b1a06c6dee3->leave($__internal_2b6e3934cfdf39899289762d6ed98143b3c4c80042b0777f096d0b1a06c6dee3_prof);
+        $__internal_8be59edeb7c103ee4239d9248feb45ef5d2fe6c51fe8501025790fc5f29667cf->leave($__internal_8be59edeb7c103ee4239d9248feb45ef5d2fe6c51fe8501025790fc5f29667cf_prof);
 
     }
 
@@ -49,14 +51,16 @@ div.header {
     }
 }
 /* <style>*/
-/* div.page {*/
-/* 	*/
+/* body {*/
+/* 	background-color: #e9eaed;*/
+/* */
 /* */
 /* }*/
 /* div.header {*/
+/* 	font-family:Calibri;*/
 /* 	padding-bottom: 200;*/
 /* 	width: 800;*/
-/* 	background-color: lightblue;*/
+/* 	background-color: #ffffff;*/
 /* 	text-align: justify;*/
 /* }*/
 /* */
--- src/CarRent/Header.php
+++ src/CarRent/Header.php
@@ -10,13 +10,16 @@ class Header {
 		$button1 = "http://v-ie.uek.krakow.pl/~s181008/przycisk1.jpg";
 		$button2 = "http://v-ie.uek.krakow.pl/~s181008/przycisk2.jpg";
 		$button3 = "http://v-ie.uek.krakow.pl/~s181008/przycisk3.jpg";
+		$blank = "http://v-ie.uek.krakow.pl/~s181008/blank.jpg";
 		
 		echo "<head><meta charset='UTF-8'><title>Wypo¿yczalnia Rycerze Jedi</title></head><body>";
 		echo "<div class='page'><center><div class='header'>";
 		echo "<center><a href='$homepage'><img src='$baner' alt='BANER' width='600' height='170' align='center'></a><br>";
+		echo "<img src='$blank'>";
 		echo "<a href='$homepage/lista'><img src='$button1' alt='PRZYCISK1'></a>";
 		echo "<a href='$homepage/rezerwacja'><img src='$button2' alt='PRZYCISK2'></a>";
-		echo "<a href='$homepage/kontakt'><img src='$button3' alt='PRZYCISK3'></a></center><BR>";
+		echo "<a href='$homepage/kontakt'><img src='$button3' alt='PRZYCISK3'></a>";
+		echo "<img src='$blank'></center><BR>";
 
 
 
