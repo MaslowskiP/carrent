e090c11b6ef222842c64246d4bb30bcf48ce4bd6
“Wojciech <wojtek.wloch12@gmail.com>
2016-02-27 18:18:03 +0100

    Dodanie segmentow do list samochodow

--- src/CarRent/Lista.php
+++ src/CarRent/Lista.php
@@ -15,7 +15,7 @@ class Lista {
 			echo '<h1>Lista samochodów:</h1>'; 
 			$cars = $asd->carlist->car; 
 			echo "<center><table border='1'>";
-			echo "<tr><td>Marka</td><td>Model</td><td>Rocznik</td><td>Kolor</td><td>Cena za dzień</td></tr>";
+			echo "<tr><td>Marka</td><td>Model</td><td>Rocznik</td><td>Kolor</td><td>Segment</td><td>Cena za dzień</td></tr>";
 			foreach($cars as $car) 
 			{ 
 				$brand = $car->brand; 
@@ -24,8 +24,9 @@ class Lista {
 				$color = $car->color;
 				$class = $car->class; 
 				$cost = $car->cost;  
+				$class = $car->class;
 
-				echo '<tr><td>'.$brand.'</td><td>'.$model.'</td><td>'.$year.'</td><td>'.$color.'</td><td>'.$cost.' zł</td></tr>';
+				echo '<tr><td>'.$brand.'</td><td>'.$model.'</td><td>'.$year.'</td><td>'.$color.'</td><td>'.$class.'</td><td>'.$cost.' zł</td></tr>';
 
 			} 
 		}
--- src/CarRent/Rezerwacja.php
+++ src/CarRent/Rezerwacja.php
@@ -13,7 +13,7 @@ class Rezerwacja {
 			echo '<h1>Wybierz samochód:</h1>'; 
 			$cars = $asd->carlist->car; 
 			echo "<center><table border='1'>";
-			echo "<tr><td>Marka</td><td>Model</td><td>Rocznik</td><td>Kolor</td><td>Cena za dzień</td><td>Aktualny status samochodu</td></tr>";
+			echo "<tr><td>Marka</td><td>Model</td><td>Rocznik</td><td>Kolor</td><td>Segment</td><td>Cena za dzień</td><td>Aktualny status samochodu</td></tr>";
 			$dzisiaj = date('d-m-Y H:i'); 
 			foreach($cars as $car) 
 				{ 
@@ -25,6 +25,7 @@ class Rezerwacja {
 				$cost = $car->cost; 
 				$status = $car->status; 
 				$date = $car->date;
+				$class = $car->class;
 				if ($status == 1 && $dzisiaj > $date)
 				{
 					$status = "<a href='http://v-ie.uek.krakow.pl/~s181008/app_dev.php/akceptacja/$id'>Rezerwuj samochód</a>";
@@ -32,7 +33,7 @@ class Rezerwacja {
 					$status = "Zarezerwowany do $date";
 				}
 					
-				echo '<tr><td>'.$brand.'</td><td>'.$model.'</td><td>'.$year.'</td><td>'.$color.'</td><td>'.$cost.' zł</td><td>'.$status.'</td></tr>';
+				echo '<tr><td>'.$brand.'</td><td>'.$model.'</td><td>'.$year.'</td><td>'.$color.'</td><td>'.$class.'</td><td>'.$cost.' zł</td><td>'.$status.'</td></tr>';
 				}
 			}
 	}		
